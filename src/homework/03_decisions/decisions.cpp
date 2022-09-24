8d651855ac43562c32bd6a8640e2e7d0e833d3dc
noahprich <bossnoah@ymail.com>
2022-09-24 19:52:10 +0000

    All I have for this assignment so far

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,42 @@
 //write include statement for decisions header
+#include <iostream>
+#include <string>
+#include "decisions.h"
 
+using std::cout;
+using std::cin;
+using std::string;
 
 //Write code for function(s) code here
+std::string get_letter_grade_using_if (int grade)
+{
+    if (grade >= 90){
+        cout<<"Your grade: A "<<grade<<"/n";
+    }
+    else if (grade >=80){
+        cout<<"Your grade: B "<<grade<<"/n";
+    }
+    else if (grade >=70){
+        cout<<"Your grade: B "<<grade<<"/n";
+    }
+    else if (grade >=60){
+        cout<<"Your grade: B "<<grade<<"/n";
+    }
+    else if (grade <= 59){
+        cout<<"Your grade: F "<<grade<<"/n";
+    }
+    else {
+        cout<<"That is not a valid grade input. Try again..."<<"/n";
+    }
+    return 0;
+}
+
+std::string get_letter_grade_using_switch (int grade)
+{
+    switch (grade) 
+    {
+        case 
+    }
+
+    return 0;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,35 @@
 //write include statements
 
+#include <iostream>
+#include <string>
+#include "decisions.h"
+
+using std::cout;
+using std::cin;
+
 int main() 
 {
+	
+	cout<<"Main Menu"<<"/n"
+		<<"1 - Letter grade using if"
+		<<"2 - Letter grade using switch"
+		<<"3 - Exit";
+	cin>>int choice;
+
+	if (choice == 1)
+	{
+		get_letter_grade_using_if(int grade);
+	}
+
+	else if (choice == 2)
+	{
+		get_letter_grade_using_switch(int grade);
+	}
+
+	else
+	{
+		cout<<"Goodbye"<<"/n";
+	}
+
 	return 0;
 }
