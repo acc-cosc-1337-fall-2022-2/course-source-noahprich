46ac28c51d3ec39f0efcae4e55bf620b63b20c71
noahprich <noah.richardson@g.austincc.edu>
2022-11-09 21:39:04 -0600

    bank account example

--- src/examples/08_classes/02_shapes/main.cpp
+++ src/examples/08_classes/02_shapes/main.cpp
@@ -11,8 +11,23 @@ Create vector of Shape pointers
 iterate with auto
 */
 
+using std::vector; using std::cout;
+
 int main() 
 {
+	const auto SIZE = 3;
 
+	Circle circles[SIZE];
+
+	circles[0].draw();
+	circles[1].draw();
+	circles[2].draw();
+
+	Line lines[SIZE];
+	lines[0].draw();
+
+	Shape* shape = circles;
+	shape->draw();
+	
 	return 0;
 }
